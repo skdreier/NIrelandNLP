ff6aafa4ec4a82a6b5491c1e9a8d08da805bde44
SKDreier <skdreier@uw.edu>
2020-01-07 14:04:58 -0800

    Updated script w SKD's pathway

--- initial_script.R
+++ initial_script.R
@@ -1,19 +1,21 @@
 rm(list=ls())
 
 library(readr)
+library(tidyverse)
+library(stringr)
+
+jose <- F
 
 if(jose == T) {
 path <- "/Users/josehernandez/Documents/eScience/projects/NIreland_NLP/justifications_01-06/justifications_txt/"
 text_fn <- "J_Denial.txt"
-} else {
-  
+} else {path <- "/Users/sarahdreier/OneDrive/Academic/repositories/NIreland_NLP/justifications_01-06/justifications_txt/"
+text_fn <- "J_Denial.txt"
 }
 
-mystring <- read_file("/Users/josehernandez/Documents/eScience/projects/NIreland_NLP/justifications_01-06/justifications_txt/J_Denial.txt")
+mystring <- read_file(text_fn)
 
 mystring
-library(tidyverse)
-library(stringr)
 
 string <- "Files\\\\DNV_round1\\\\DEFE 13 1358\\\\IMG_9711_DEFE_13_1358"
  
