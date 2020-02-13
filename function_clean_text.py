ff973017008fa44c484574a10a926cb83a7db90d
SKDreier <skdreier@uw.edu>
2020-02-13 09:52:26 -0800

    Function to clean text and OHE justification categories

--- function_clean_text.py
+++ function_clean_text.py
@@ -1,4 +1,5 @@
 # This script builds a function to clean the data! 
+# SKD Feb 13
 
 import os
 from pathlib import Path
@@ -10,14 +11,14 @@ this_file_path = os.path.abspath(__file__)
 project_root = os.path.split(this_file_path)[0]
 train_path = os.path.join(project_root) + '/'
 
-df = pd.read_csv(os.path.join(train_path, "justifications_long_training.csv"))
+df_just = pd.read_csv(os.path.join(train_path, "justifications_long_training.csv"))
+df_just.shape
 
-#df_test = df.iloc[0:10, 2:3]
-#df_test = df[0:10]
-df_test = df.iloc[[22, 30, 85, 130, 131, 157, 159, 177, 194, 239, 240, 241], :] # combo of messed up, all caps, and normal text
-df_test.columns
-
-# Do one hot encoding code before running this function
+# Sample text to test function
+#df_test = df_just.iloc[0:10, 2:3]
+#df_test = df_just[0:10]
+#df_test = df_just.iloc[[22, 30, 85, 130, 131, 157, 159, 177, 194, 239, 240, 241], :] # combo of messed up, all caps, and normal text
+#df_test.columns
 
 # Function to clean text and return it as either a dataframe or a text list
 def clean_func(column, df, analysis = False):
@@ -31,9 +32,12 @@ def clean_func(column, df, analysis = False):
         list_clean_col = list(new_col)
         return(list_clean_col)
 
-clean_func(column = df_test['text'], df = df_test) # Default analysis = false
+clean_func(column = df_just['text'], df = df_just) # Default analysis = false
 #clean_func(column = df_test['text'], df = df_test, analysis = True)
 
-# Run the function on whole dataset
+# One hot encoding after running this function
+cat_columns = ['justification']
+df_just = pd.get_dummies(df_just, columns=cat_columns)
 
-# Save it as a csv.
+# Save it as a csv
+df_just.to_csv(os.path.join(project_root, 'justifications_clean_text_ohe.csv'))
