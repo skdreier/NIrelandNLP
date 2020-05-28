0a44e9e35f6b6d39c848ab7f160bba0f2750c010
SKDreier <skdreier@uw.edu>
2020-05-27 23:37:22 -0700

    w2v plot for pgms

--- class_nn/embedding_viz.py
+++ class_nn/embedding_viz.py
@@ -110,3 +110,46 @@ def tsne_plot_similar_words(title, labels, embedding_clusters, word_clusters, a,
 
 tsne_plot_similar_words('Archive Corpus Word Embeddings', keys, embeddings_en_2d, word_clusters, 0.7,
                         'figures/similar_words_for_online.png')
+
+
+### Plot for PGM analysis
+keys = ['uda', 'uvf']
+
+embedding_clusters = []
+word_clusters = []
+for word in keys:
+    embeddings = []
+    words = []
+    for similar_word, _ in w_vec_model.most_similar(word, topn=10):
+        words.append(similar_word)
+        embeddings.append(w_vec_model[similar_word])
+    embedding_clusters.append(embeddings)
+    word_clusters.append(words)
+
+embedding_clusters = np.array(embedding_clusters)
+n, m, k = embedding_clusters.shape
+
+tsne_model_en_2d = TSNE(perplexity=15, n_components=2, init='pca', n_iter=3500, random_state=32)
+embeddings_en_2d = np.array(tsne_model_en_2d.fit_transform(embedding_clusters.reshape(n * m, k))).reshape(n, m, 2)
+
+def tsne_plot_similar_words(title, labels, embedding_clusters, word_clusters, a, filename):
+    plt.figure(figsize=(16, 9))
+    colors = cm.Dark2(np.linspace(0, 1, len(labels)))
+    for label, embeddings, words, color in zip(labels, embedding_clusters, word_clusters, colors):
+        x = embeddings[:, 0]
+        y = embeddings[:, 1]
+        plt.scatter(x, y, c=color, alpha=a, label=label)
+        for i, word in enumerate(words):
+            plt.annotate(word, alpha=0.5, xy=(x[i], y[i]), xytext=(5, 2),
+                         textcoords='offset points', ha='right', va='bottom', size=12)
+    plt.legend(loc=4)
+    plt.title(title)
+    plt.grid(True)
+    if filename:
+        plt.savefig(filename, format='png', dpi=150, bbox_inches='tight')
+    plt.show()
+
+
+tsne_plot_similar_words('Archive Corpus Word Embeddings', keys, embeddings_en_2d, word_clusters, 0.7,
+                        'figures/similar_words_pgm.png')
+
